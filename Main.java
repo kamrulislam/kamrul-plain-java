e1c06bf1c6d5b39c6ad4425cd7b54e6361270aeb
Md Kamrul Islam <kamruli@gmail.com>
2019-09-24 01:37:19 +1000

    Moved Test runner on the top level

--- Main.java
+++ Main.java
@@ -1,37 +1,34 @@
-package kamrul;
-
-import java.util.List;
-import java.util.ArrayList;
-
+import kamrul.ICsvReader;
+import kamrul.account.Balance;
 import kamrul.file.FileReader;
 import kamrul.transaction.Transaction;
 import kamrul.transaction.TransactionProcessor;
-import kamrul.account.Balance;
 
+import java.util.ArrayList;
+import java.util.List;
 
-
-public class KamrulPlainJavaApplication {
+public class Main {
 
     public static void main(String[] args) {
+
+
         //input processing
         ICsvReader csvReader = new FileReader("./input.csv");
         List<String> result = csvReader.getDataFromCsv();
         List<Transaction> transactions = new ArrayList<Transaction>();
         result.forEach((strData) -> transactions.add((new Transaction()).fromStringCsv(strData)));
-        
+
 
         //process
         TransactionProcessor processor = new TransactionProcessor(transactions);
 
         Balance balance = processor.calculateRelativeAccountBalance(
-            "ACC334455",
-            "20/10/2018 12:00:00",
-            "20/10/2018 19:00:00"
+                "ACC334455",
+                "20/10/2018 12:00:00",
+                "20/10/2018 19:00:00"
         );
 
         System.out.println("Relative balance for the period is: " + balance.getAmount());
         System.out.println("Number of transactions included is: " + balance.getNumberOfTransactions());
     }
 }
-
-
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!bin/bash
 
-javac kamrul/KamrulPlainJavaApplication.java && java kamrul/KamrulPlainJavaApplication
+javac Main.java && java Main
